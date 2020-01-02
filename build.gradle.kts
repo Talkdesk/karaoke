608835d9c27980ba8d839951a153a461abafb37c
Erick Cordeiro Ozaki <erick.ozaki@talkdesk.com>
2020-01-02 16:14:47 +0000

    Adding Twilio dependency to project
    Ref:
     - https://www.twilio.com/docs/libraries/java
     - https://stackoverflow.com/a/53815926

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation(group = "com.twilio.sdk", name = "twilio", version = "7.42.0")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
