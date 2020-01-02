c240a433d21fa37d49563f91588525a46eac7c0c
Erick Cordeiro Ozaki <erick.ozaki@talkdesk.com>
2020-01-02 18:16:05 +0000

    Removing needless imports from InteractionControllerTest
    Imports were suggested by IntelliJ in previous versions, but never
    rechecked.

--- src/test/kotlin/com/karaoke/orchestrator/InteractionControllerTest.kt
+++ src/test/kotlin/com/karaoke/orchestrator/InteractionControllerTest.kt
@@ -1,17 +1,10 @@
 package com.karaoke.orchestrator
 
-import org.assertj.core.api.Assertions.assertThat
-import org.json.JSONArray
-import org.json.JSONObject
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.web.client.TestRestTemplate
-import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.http.MediaType
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc;
