e3b3c03a2c675cacd02c270ab2127c9cea426c71
Erick Cordeiro Ozaki <erick.ozaki@talkdesk.com>
2020-01-02 18:20:25 +0000

    Creating TwilioAuth abstract class
    Class holds global vars for Twilio auth params

---
+++ src/main/kotlin/com/karaoke/orchestrator/TwilioAuth.kt
@@ -0,0 +1,8 @@
+package com.karaoke.orchestrator
+
+abstract class TwilioAuth() {
+  companion object {
+    val ACCOUNT_SID: String = System.getenv("ACCOUNT_SID")
+    val AUTH_TOKEN: String = System.getenv("AUTH_TOKEN")
+  }
+}
