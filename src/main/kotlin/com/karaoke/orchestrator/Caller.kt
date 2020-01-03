1c988b8a1db04ea96969e0b2e4ab675dd78413e0
Erick Cordeiro Ozaki <erick.ozaki@talkdesk.com>
2020-01-03 11:12:19 +0000

    Creating Calles abstract class
    Class should handle calling functions

---
+++ src/main/kotlin/com/karaoke/orchestrator/Caller.kt
@@ -0,0 +1,9 @@
+import com.twilio.Twilio;
+import com.twilio.http.HttpMethod;
+import com.twilio.rest.api.v2010.account.Call;
+import com.twilio.type.PhoneNumber;
+
+import java.net.URI;
+
+abstract class Caller() {
+}
