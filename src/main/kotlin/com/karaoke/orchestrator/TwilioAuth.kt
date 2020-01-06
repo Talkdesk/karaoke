72f8c91a3aec0a5ced100d253abf78c3d4ce5957
Erick Cordeiro Ozaki <erick.ozaki@talkdesk.com>
2020-01-06 13:09:23 +0000

    Adding origin phone number to Twilio Auth class

--- src/main/kotlin/com/karaoke/orchestrator/TwilioAuth.kt
+++ src/main/kotlin/com/karaoke/orchestrator/TwilioAuth.kt
@@ -4,5 +4,6 @@ abstract class TwilioAuth() {
   companion object {
     val ACCOUNT_SID: String = System.getenv("ACCOUNT_SID")
     val AUTH_TOKEN: String = System.getenv("AUTH_TOKEN")
+    val FROM_NUMBER: String = System.getenv("FROM_NUMBER")
   }
 }
